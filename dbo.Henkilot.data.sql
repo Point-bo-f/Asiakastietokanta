26a0b0bff97c8e9c4dfbc8ed9239123ed08aae7e
Bo Furustam <bo.furustam@stud.pointcollege.fi>
2018-01-05 11:50:52 +0200

    Nimi>Projektinimi

---
+++ dbo.Henkilot.data.sql
@@ -0,0 +1,3 @@
+﻿SET IDENTITY_INSERT [dbo].[Henkilot] ON
+INSERT INTO [dbo].[Henkilot] ([HenkiloID], [Etunimi], [Sukunimi], [Osoite], [Esimies]) VALUES (1002, N'Kalle', N'Kokeilija', N'Testiosoite', 1001)
+SET IDENTITY_INSERT [dbo].[Henkilot] OFF
